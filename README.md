3cab7296a24be58ad726c29e8ae7df1073bcc701
Wes Griffin <themisguidedidiot@gmail.com>
2019-06-26 11:55:54 -0700

    about half way through assignment. horribly named commit.

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # oops-files
+
+
+#Goal
+    -rename files based on the "word content", number in file and date last modified.
+
+##Steps
+
+1. recreate the problem.
+2. get all files in target directory.
+3. figure out to rename a file
+    1. get last date modified
+4. get all files
+5. rename all files as 'content-oldFileNumber-modifiedDate' 
--- index.js
+++ index.js
@@ -1,15 +1,45 @@
-
 const fs = require('fs');
-const directoryPath = './old-files';
+const directoryPath = './new-files';
 
-fs.readdir(directoryPath, function (err, files) {
-    if (err) {
-        return console.log('Unable to scan directory: ' + err);
-    } 
-    console.log(files);
-    files.forEach((file) => {
-        fs.rename(file, 'newFile.txt' (err) => {
-            if (err) console.log(err);
-        })
+const animals = [
+  'dog',
+  'leopard',
+  'baboon',
+  'lemur',
+  'koala',
+  'gorilla',
+];
+
+const getAnimal = () => {
+  const index = Math.floor(Math.random() * animals.length);
+  return animals[index];
+};
+
+
+const createFiles = (directory, fileCount, callback) => {
+  let writtenSoFar = 0;
+  for(let i = 0; i < fileCount; i++) {
+    fs.writeFile(`${directory}/${i}.txt`, getAnimal(), (err) => {
+      if(err) return callback(err);
+      writtenSoFar += 1;
+
+      if(writtenSoFar === count) callback();
+
     });
-});
+  }
+};
+
+// fs.readdir(directoryPath, function(err, files) {
+//   if(err) {
+//     return console.log('Unable to scan directory: ' + err);
+//   } 
+//   console.log(files);
+//   files.forEach((file) => {
+//     fs.rename(file, 'newFile.txt', (err) => {
+//       if(err) console.log(err);
+//     });
+//   });
+// });
+
+
+module.exports = { getAnimal, createFiles };
